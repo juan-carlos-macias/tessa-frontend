9d959f5cbf011909a417335b81c1e8b53bd64005
Juan Carlos Macias <maciasnajeraj@gmail.com>
2025-11-17 15:19:53 -0600

    test: add mock fot firebase auth

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,20 @@ import 'whatwg-fetch'
 
 // Mock Firebase
 global.fetch = jest.fn()
+
+// Mock Firebase modules to prevent initialization errors in CI
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(() => ({})),
+  getApps: jest.fn(() => []),
+  getApp: jest.fn(() => ({})),
+}))
+
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(() => ({})),
+  signInWithEmailAndPassword: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn(),
+  signInWithPopup: jest.fn(),
+  GoogleAuthProvider: jest.fn(),
+  signOut: jest.fn(),
+  onAuthStateChanged: jest.fn(),
+}))
